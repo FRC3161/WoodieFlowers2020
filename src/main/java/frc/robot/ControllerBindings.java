5926c6d8bd46bd5b1cf0ac81f7b382650dc95af2
David Drobner <david@drobner.ca>
2020-01-09 21:49:00 -0500

    Created ControllerBindings.java

---
+++ src/main/java/frc/robot/ControllerBindings.java
@@ -0,0 +1,7 @@
+package frc.robot;
+
+import ca.team3161.lib.utils.controls.LogitechDualAction.LogitechControl;
+
+public final class ControllerBindings {
+    public static final LogitechControl driverStick = LogitechControl.LEFT_STICK;
+}
