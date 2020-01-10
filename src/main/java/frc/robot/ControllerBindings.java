270cb538dd9c0a0f159f992b74aee283bbfda31d
David Drobner <david@drobner.ca>
2020-01-09 22:12:06 -0500

    Updated variable names to conform to convention

--- src/main/java/frc/robot/ControllerBindings.java
+++ src/main/java/frc/robot/ControllerBindings.java
@@ -3,6 +3,6 @@ package frc.robot;
 import ca.team3161.lib.utils.controls.LogitechDualAction.LogitechControl;
 
 public final class ControllerBindings {
-    public static final LogitechControl driveLeftStick = LogitechControl.LEFT_STICK;
-    public static final LogitechControl driveRightStick = LogitechControl.RIGHT_STICK;
+    public static final LogitechControl DRIVE_LEFT_STICK = LogitechControl.LEFT_STICK;
+    public static final LogitechControl DRIVE_RIGHT_STICK = LogitechControl.RIGHT_STICK;
 }
