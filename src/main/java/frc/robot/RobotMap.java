47a95b415da909d3eb5fbb5618f98d33e6a77a65
David Drobner <david@drobner.ca>
2020-01-15 21:49:55 -0500

    Created Encoders

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -4,4 +4,8 @@ public final class RobotMap {
     public static final int TALON_LEFT_DRIVE_PORT = 0; //TODO
     public static final int TALON_RIGHT_DRIVE_PORT = 1; //TODO
     public static final int DRIVER_PAD_PORT = 0;
+
+    //Encoders
+    public static final int[] LEFT_ENCODER_PORTS = {0, 1}; //TODO
+    public static final int[] RIGHT_ENCODER_PORTS = {2, 3}; //TODO
 }
--- src/main/java/frc/robot/subsystems/drivetrain/DrivetrainImpl.java
+++ src/main/java/frc/robot/subsystems/drivetrain/DrivetrainImpl.java
@@ -1,19 +1,25 @@
 package frc.robot.subsystems.drivetrain;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import frc.robot.RobotMap;
-
+import edu.wpi.first.wpilibj.Encoder;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public class DrivetrainImpl implements Drivetrain {
+    //Speed controllers and drivetrain
     private final WPI_TalonSRX leftMotorController;
     private final WPI_TalonSRX rightMotorController;
     private final DifferentialDrive drivetrain;
     
+    //Encoders
+    private final Encoder leftEncoder;
+    private final Encoder rightEncoder;
+    
     public DrivetrainImpl(){
-        this.leftMotorController = new WPI_TalonSRX(RobotMap.TALON_LEFT_DRIVE_PORT); //TODO Placeholder Channel
-        this.rightMotorController = new WPI_TalonSRX(RobotMap.TALON_RIGHT_DRIVE_PORT); //TODO Placeholder Channel
+        this.leftMotorController = new WPI_TalonSRX(frc.robot.RobotMap.TALON_LEFT_DRIVE_PORT); //TODO Placeholder Channel
+        this.rightMotorController = new WPI_TalonSRX(frc.robot.RobotMap.TALON_RIGHT_DRIVE_PORT); //TODO Placeholder Channel
         this.drivetrain = new DifferentialDrive(this.leftMotorController, this.rightMotorController);
+        this.leftEncoder = new Encoder(frc.robot.RobotMap.LEFT_ENCODER_PORTS[0], frc.robot.RobotMap.LEFT_ENCODER_PORTS[1]);
+        this.rightEncoder = new Encoder(frc.robot.RobotMap.RIGHT_ENCODER_PORTS[0], frc.robot.RobotMap.RIGHT_ENCODER_PORTS[1]);
     }
 
     @Override
