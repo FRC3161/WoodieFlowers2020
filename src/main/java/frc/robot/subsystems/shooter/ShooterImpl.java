a1ee9938218b2aaf01c9f6c841ee6d44cf272dad
David Drobner <david@drobner.ca>
2020-01-16 16:05:20 -0500

    Create skeleton shooter class

---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterImpl.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems.shooter;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+public class ShooterImpl  implements Shooter{
+    public void shoot() {
+
+    }
+}
