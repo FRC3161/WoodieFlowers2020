17e13a14d649d683d198a456a75b95e09e978752
David Drobner <david@drobner.ca>
2020-01-16 16:14:16 -0500

    Created basic shoot method

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,16 +1,20 @@
 package frc.robot;
 
 public final class RobotMap {
+    // Drive
     public static final int TALON_LEFT_DRIVE_PORT = 0; //TODO
     public static final int TALON_RIGHT_DRIVE_PORT = 1; //TODO
     public static final int DRIVER_PAD_PORT = 0;
 
-    //Encoders
+    // Encoders
     public static final int[] LEFT_ENCODER_PORTS = {0, 1}; //TODO
     public static final int[] RIGHT_ENCODER_PORTS = {2, 3}; //TODO
 
-    //PID
+    // PID
     public static final double Kp = 0.0001;
     public static final double Ki = 0.0001;
     public static final double Kd = 0.0001;
+
+    // Shooter
+    public static final int SHOOTER_TALON_PORT = 2; // TODO
 }
--- src/main/java/frc/robot/subsystems/shooter/ShooterImpl.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterImpl.java
@@ -3,7 +3,21 @@ package frc.robot.subsystems.shooter;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public class ShooterImpl  implements Shooter{
+    
+    WPI_TalonSRX shooterController;
+    
+    public ShooterImpl() {
+        this.shooterController  = new WPI_TalonSRX(frc.robot.RobotMap.SHOOTER_TALON_PORT);
+    }
+    
     public void shoot() {
-
+        // Right now this is a placeholder to allow us to test
+        this.shooterController.set(1.0d);
+        try{
+            Thread.sleep(5000);
+        } catch(InterruptedException e){
+            System.out.println("Shooting");
+        }
+        this.shooterController.set(0.0d);
     }
 }
