b27dc5ea7099e009f1b3a3d0833124bb1e6cac77
David Drobner <david@drobner.ca>
2020-01-20 11:49:19 -0500

    Remove encoders and PIDControllers from DrivetrainImpl

--- src/main/java/frc/robot/subsystems/drivetrain/DrivetrainImpl.java
+++ src/main/java/frc/robot/subsystems/drivetrain/DrivetrainImpl.java
@@ -11,25 +11,11 @@ public class DrivetrainImpl implements Drivetrain {
     private final WPI_TalonSRX rightMotorController;
     private final DifferentialDrive drivetrain;
     
-    //Encoders
-    private final Encoder leftEncoder;
-    private final Encoder rightEncoder;
-    
-    //PID
-    private final PIDController leftPIDController;
-    private final PIDController rightPIDController;
-    
     public DrivetrainImpl(){
         //Motor controllers and drivetrain
         this.leftMotorController = new WPI_TalonSRX(frc.robot.RobotMap.TALON_LEFT_DRIVE_PORT); //TODO Placeholder Channel
         this.rightMotorController = new WPI_TalonSRX(frc.robot.RobotMap.TALON_RIGHT_DRIVE_PORT); //TODO Placeholder Channel
         this.drivetrain = new DifferentialDrive(this.leftMotorController, this.rightMotorController);
-        //Encoders
-        this.leftEncoder = new Encoder(frc.robot.RobotMap.LEFT_ENCODER_PORTS[0], frc.robot.RobotMap.LEFT_ENCODER_PORTS[1]);
-        this.rightEncoder = new Encoder(frc.robot.RobotMap.RIGHT_ENCODER_PORTS[0], frc.robot.RobotMap.RIGHT_ENCODER_PORTS[1]);
-        //PID
-        this.leftPIDController = new PIDController(frc.robot.RobotMap.Kp, frc.robot.RobotMap.Ki, frc.robot.RobotMap.Kd);
-        this.rightPIDController = new PIDController(frc.robot.RobotMap.Kp, frc.robot.RobotMap.Ki, frc.robot.RobotMap.Kd);
     }
 
     @Override
