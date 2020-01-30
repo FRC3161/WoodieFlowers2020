e807c0d3c5e3aa266229cfe805ba2415eb88752c
David Drobner <david@drobner.ca>
2020-01-29 22:49:53 -0500

    Implemented skeleton Feeder subsystem

---
+++ src/main/java/frc/robot/subsystems/ball/feeder/FeederImpl.java
@@ -0,0 +1,37 @@
+package frc.robot.subsystems.ball.feeder;
+
+import ca.team3161.lib.robot.subsystem.RepeatingPooledSubsystem;
+import java.util.concurrent.TimeUnit;
+
+public class FeederImpl extends RepeatingPooledSubsystem implements Feeder {
+    
+    FeederImpl(){
+        super(1, TimeUnit.SECONDS);
+        // PLACEHOLDER
+    }
+
+    public void feedBalls() {
+        // PLACEHOLDER
+        return;
+    }
+
+    public void stop() {
+        // PLACEHOLDER
+        return;
+    }
+
+    public void reverseFeeder() {
+        // PLACEHOLDER
+        return;
+    }
+
+    public void defineResources(){
+        // PLACEHOLDER
+        return;
+    }
+
+    public void task() {
+        // PLACEHOLDER
+        return;
+    }
+}
