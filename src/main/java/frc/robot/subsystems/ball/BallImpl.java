66c9e4ca630b8aecc5c435041115f93b1804cb96
David Drobner <david@drobner.ca>
2020-01-29 22:49:44 -0500

    Implemented skeleton Ball subsystem

---
+++ src/main/java/frc/robot/subsystems/ball/BallImpl.java
@@ -0,0 +1,32 @@
+package frc.robot.subsystems.ball;
+
+import ca.team3161.lib.robot.subsystem.RepeatingPooledSubsystem;
+import java.util.concurrent.TimeUnit;
+
+public class BallImpl extends RepeatingPooledSubsystem implements Ball {
+    
+    BallImpl() {
+        // PLACEHOLDER
+        super(1, TimeUnit.SECONDS);
+    }
+    
+    public void shoot(){
+        // PLACEHOLDER
+        return;
+    }
+
+    public void cancelShoot(){
+        //PLACEHOLDER
+        return;
+    }
+
+    public void defineResources(){
+        // PLACEHOLDER
+        return;
+    }
+
+    public void task() {
+        // PLACEHOLDER
+        return;
+    }
+}
