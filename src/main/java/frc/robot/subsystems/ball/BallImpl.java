68db953ed15f2d3b5a29d0b26659a55e428a32a6
David Drobner <david@drobner.ca>
2020-01-29 23:52:43 -0500

    Created Feeder and Shooter in BallImpl

--- src/main/java/frc/robot/subsystems/ball/BallImpl.java
+++ src/main/java/frc/robot/subsystems/ball/BallImpl.java
@@ -3,11 +3,19 @@ package frc.robot.subsystems.ball;
 import ca.team3161.lib.robot.subsystem.RepeatingPooledSubsystem;
 import java.util.concurrent.TimeUnit;
 
+import frc.robot.subsystems.ball.feeder.FeederImpl;
+import frc.robot.subsystems.ball.shooter.ShooterImpl;
+
 public class BallImpl extends RepeatingPooledSubsystem implements Ball {
+
+    ShooterImpl shooter;
+    FeederImpl feeder;
     
     BallImpl() {
         // PLACEHOLDER
         super(1, TimeUnit.SECONDS);
+        this.shooter = new ShooterImpl();
+        this.feeder = new FeederImpl();
     }
     
     public void shoot(){
