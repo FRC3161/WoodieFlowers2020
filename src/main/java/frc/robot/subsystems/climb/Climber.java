deea49aa0d2845e823c3d217487185ab1ce76dad
David Drobner <david@drobner.ca>
2020-01-31 23:38:37 -0500

    Create skeleton Climber class

--- src/main/java/frc/robot/subsystems/climb/Climber.java
+++ src/main/java/frc/robot/subsystems/climb/Climber.java
@@ -2,4 +2,7 @@ package frc.robot.subsystems.climb;
 
 public interface Climber {
     public void move(char direction);
+    void extendClimber();
+    void retractClimber();
+    void liftRobot();
 }
