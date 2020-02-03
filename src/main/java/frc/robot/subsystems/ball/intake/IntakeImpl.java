ba11f8d86159e1004d5c08e8c85869aab7bfeabb
David Drobner <david@drobner.ca>
2020-02-03 15:47:12 -0500

    Created Intake interface and IntakeImpl class

---
+++ src/main/java/frc/robot/subsystems/ball/intake/IntakeImpl.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems.ball.intake;
+
+public class IntakeImpl implements Intake {
+    public void extend(boolean motorDirection) {
+        // Placeholder
+        return;
+    }
+
+    public void extend() {
+        extend(true);
+    }
+
+    public void retract() {
+        // Placeholder
+        return;
+    }
+}
