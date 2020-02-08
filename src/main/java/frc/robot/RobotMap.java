925e370104d86440e1dc0225927855f9f6e95292
David Drobner <david@drobner.ca>
2020-02-08 15:28:37 -0500

    Remove unnecessary comments from RobotMap

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -4,14 +4,14 @@ public final class RobotMap {
     // TODO gotta fix all of these, it's pretty much just random numbers at this point
     
     // Drive
-    public static final int TALON_LEFT_DRIVE_PORT = 0; //TODO
-    public static final int TALON_RIGHT_DRIVE_PORT = 1; //TODO
+    public static final int TALON_LEFT_DRIVE_PORT = 0; 
+    public static final int TALON_RIGHT_DRIVE_PORT = 1; 
     public static final int DRIVER_PAD_PORT = 0;
     public static final int OPERATOR_PAD_PORT = 1;
 
     // Encoders
-    public static final int[] LEFT_ENCODER_PORTS = {0, 1}; //TODO
-    public static final int[] RIGHT_ENCODER_PORTS = {2, 3}; //TODO
+    public static final int[] LEFT_ENCODER_PORTS = {0, 1}; 
+    public static final int[] RIGHT_ENCODER_PORTS = {2, 3}; 
 
     // PID
     public static final double Kp = 0.0001;
@@ -19,10 +19,10 @@ public final class RobotMap {
     public static final double Kd = 0.0001;
 
     // Shooter
-    public static final int SHOOTER_TALON_PORT = 2; // TODO
+    public static final int SHOOTER_TALON_PORT = 2; 
     public static final int ROLLER_TALON_PORT = 3;
     public static final int BELT_TALON_PORT = 4;
-    public static final int SHOOTER_SOLENOID_CHANNEL = 0; //TODO
+    public static final int SHOOTER_SOLENOID_CHANNEL = 0; 
     public static final int[] SHOOTER_ENCODER_PORTS = {4, 5};
 
     // Intake
@@ -31,7 +31,7 @@ public final class RobotMap {
     public static final int INTAKE_SOLENOID_OUT_CHANNEL = 3;
 
     // Climber
-    public static final int CLIMBER_TALON_PORT = 3; // TODO
+    public static final int CLIMBER_TALON_PORT = 3; 
     public static final int LIFTER_TALON_PORT = 5;
     public static final int CLIMBER_SOLENOID1_CHANNEL = 1;
     public static final int CLIMBER_SOLENOID2_CHANNEL = 2;
