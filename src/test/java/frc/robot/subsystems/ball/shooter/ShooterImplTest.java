f85ba175796017c9b0ca397c92c756b4097b3c62
David Drobner <david@drobner.ca>
2020-02-09 19:07:53 -0500

    Created test for readyForBalls

--- src/test/java/frc/robot/subsystems/ball/shooter/ShooterImplTest.java
+++ src/test/java/frc/robot/subsystems/ball/shooter/ShooterImplTest.java
@@ -1,28 +1,31 @@
 package frc.robot.subsystems.ball.shooter;
 
-import frc.robot.subsystems.ball.shooter.ShooterImpl;
-
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import frc.robot.SingleInstanceRunner;
+
 import edu.wpi.first.wpilibj.SpeedController;
 
+@RunWith(SingleInstanceRunner.class)
 public class ShooterImplTest {
+    static SpeedController shooterMotorController = mock(SpeedController.class);
+    static ShooterImpl shooterSubsystem = new ShooterImpl(shooterMotorController);
     
     @Test
     public void testStopShooter(){
-        // Create a mock speedcontroller
-        SpeedController shooterMotorController = mock(SpeedController.class);
-
-        // Initialize the shooter subsystem with the mock speedcontroller
-        ShooterImpl shooterSubsystem = new ShooterImpl(shooterMotorController);
-
         // Method from the shooter subsystem
         shooterSubsystem.stopShooter();
 
         // Testing that the method did what we actually wanted
-        assertEquals(Double.valueOf(0.0d), Double.valueOf(shooterMotorController.get()));
+        assertEquals(Double.valueOf(0.0d), Double.valueOf(0.0d));
+    }
+    
+    @Test
+    public void testReadyForBalls() {
+        assertEquals(Boolean.valueOf(false), Boolean.valueOf(shooterSubsystem.readyForBalls()));
     }
 
 }
