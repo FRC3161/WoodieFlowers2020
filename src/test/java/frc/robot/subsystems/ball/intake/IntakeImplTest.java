faf5c1736846bbd32f2db63f638fe3a3c5f9c152
David Drobner <david@drobner.ca>
2020-02-09 19:31:14 -0500

    Implemented testConstructor in IntakeImplTest

--- src/test/java/frc/robot/subsystems/ball/intake/IntakeImplTest.java
+++ src/test/java/frc/robot/subsystems/ball/intake/IntakeImplTest.java
@@ -1,11 +1,28 @@
 package frc.robot.subsystems.ball.intake;
 
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
 import org.junit.runner.RunWith;
+
+import frc.robot.subsystems.ball.intake.IntakeImpl;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.SpeedController;
 
 import frc.robot.SingleInstanceRunner;
 
 @RunWith(SingleInstanceRunner.class)
 public class IntakeImplTest {
+    static SpeedController intakeMotorController = mock(SpeedController.class);
+    static DoubleSolenoid intakeSolenoid = mock(DoubleSolenoid.class);
+    static IntakeImpl intakeSubsystem = mock(IntakeImpl.class);
 
+    @Test
+    public void testConstructor(){
+        // Constructor is run earlier
+        // Check the initial state of the solenoid
+        assertEquals(false, intakeSubsystem.getDeployed());
+    }
 }
