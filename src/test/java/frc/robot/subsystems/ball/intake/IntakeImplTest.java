6180aca6fb8a6000e6e3575c16ca4a9cb7e4c044
David Drobner <david@drobner.ca>
2020-02-09 19:17:26 -0500

    Create IntakeImplTest

---
+++ src/test/java/frc/robot/subsystems/ball/intake/IntakeImplTest.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems.ball.intake;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import frc.robot.SingleInstanceRunner;
+
+@RunWith(SingleInstanceRunner.class)
+public class IntakeImplTest {
+
+}
