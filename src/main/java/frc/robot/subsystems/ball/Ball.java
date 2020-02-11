d6ccfbb3107226c99abf8f41bfb26b55dcdf1bd8
David Drobner <david@drobner.ca>
2020-02-11 16:20:45 -0500

    Made Ball extend Subsystem

--- src/main/java/frc/robot/subsystems/ball/Ball.java
+++ src/main/java/frc/robot/subsystems/ball/Ball.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems.ball;
 
-public interface Ball {
+import ca.team3161.lib.robot.subsystem.Subsystem;
+
+public interface Ball extends Subsystem{
     void shoot();
     void cancelShooting();
     void stop();
