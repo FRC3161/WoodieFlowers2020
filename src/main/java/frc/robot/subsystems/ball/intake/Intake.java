a3e362c28dabaaf0ea32f45c58ae3aa16760a1dd
David Drobner <david@drobner.ca>
2020-02-11 16:23:34 -0500

    Added overloaded extend method to Intake

--- src/main/java/frc/robot/subsystems/ball/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/ball/intake/Intake.java
@@ -4,5 +4,6 @@ import ca.team3161.lib.robot.subsystem.Subsystem;
 
 public interface Intake extends Subsystem{
     void retract();
+    void extend();
     void extend(IntakeImpl.MotorDirections direction);
 }
