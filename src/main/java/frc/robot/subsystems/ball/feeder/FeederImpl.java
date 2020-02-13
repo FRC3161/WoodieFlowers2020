f69e1aab69137c79ba91cdc261afd0b061602019
David Drobner <david@drobner.ca>
2020-02-13 12:01:08 -0500

    Created ultrasonic sensor in feeder

--- src/main/java/frc/robot/subsystems/ball/feeder/FeederImpl.java
+++ src/main/java/frc/robot/subsystems/ball/feeder/FeederImpl.java
@@ -2,18 +2,23 @@ package frc.robot.subsystems.ball.feeder;
 
 import ca.team3161.lib.robot.subsystem.RepeatingPooledSubsystem;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.Ultrasonic;
 import java.util.concurrent.TimeUnit;
+import frc.robot.RobotMap;
 
 public class FeederImpl extends RepeatingPooledSubsystem implements Feeder {
     
     WPI_TalonSRX beltController;
     WPI_TalonSRX rollerController;
 
+    Ultrasonic feederUltrasonic;
+
     public FeederImpl(){
         super(1, TimeUnit.SECONDS);
         
-        this.beltController = new WPI_TalonSRX(frc.robot.RobotMap.BELT_TALON_PORT);
-        this.rollerController = new WPI_TalonSRX(frc.robot.RobotMap.ROLLER_TALON_PORT);
+        this.beltController = new WPI_TalonSRX(RobotMap.BELT_TALON_PORT);
+        this.rollerController = new WPI_TalonSRX(RobotMap.ROLLER_TALON_PORT);
+        this.feederUltrasonic = new Ultrasonic(RobotMap.ULTRASONIC_PORTS[0], RobotMap.ULTRASONIC_PORTS[1]);
     }
 
     public void feedBalls() {
