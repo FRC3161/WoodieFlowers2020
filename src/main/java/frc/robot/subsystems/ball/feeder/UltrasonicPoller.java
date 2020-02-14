2249f6a5dfcff3ea1ea8dc95de158709b1fd2043
David Drobner <david@drobner.ca>
2020-02-13 23:37:13 -0500

    Made UltrasonicPoller a subsystem

--- src/main/java/frc/robot/subsystems/ball/feeder/UltrasonicPoller.java
+++ src/main/java/frc/robot/subsystems/ball/feeder/UltrasonicPoller.java
@@ -1,13 +1,28 @@
 package frc.robot.subsystems.ball.feeder;
 
+import java.util.concurrent.TimeUnit;
+
+import ca.team3161.lib.robot.subsystem.RepeatingPooledSubsystem;
 import edu.wpi.first.wpilibj.Ultrasonic;
 
 // Pretty sure this only allows package level access
-class UltrasonicPoller {
+class UltrasonicPoller extends RepeatingPooledSubsystem {
 
     Ultrasonic ultrasonicSensor;
 
     UltrasonicPoller(Ultrasonic sensor) {
+        super(25, TimeUnit.MILLISECONDS); // 4Hz seems reasonable
         this.ultrasonicSensor = sensor;
+    }
+
+    @Override
+    public void defineResources(){
+        require(ultrasonicSensor);
+    }
+
+    @Override
+    public void task(){
+        // Placholder
+        return;
     }
 }
