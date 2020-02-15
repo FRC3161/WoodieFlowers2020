7c83c38d22d8098baafe741b57f8a0c6124634f9
David Drobner <david@drobner.ca>
2020-02-14 22:39:18 -0500

    Implemented hitNRun autonomous

--- src/main/java/frc/robot/Autonomous.java
+++ src/main/java/frc/robot/Autonomous.java
@@ -7,14 +7,19 @@ public class Autonomous {
     
     Drivetrain drivetrain;
     Ball ball;
+
+    static final double WHEEL_DIAMETER = 18.84955592;
     
     Autonomous(Drivetrain driveSubsystem, Ball ballSubsystem) {
         this.drivetrain = driveSubsystem;
         this.ball = ballSubsystem;
     }
 
-    public void hitNRun() {
-
+    public void hitNRun() throws InterruptedException {
+        this.ball.shoot();
+        Thread.sleep(5000);
+        this.ball.cancelShooting();
+        this.drivetrain.setSetpoint(-120 * (WHEEL_DIAMETER * 128)); // Distance in inches * Wheel diameter in inches * ticks per rotation
     }
 
 }
