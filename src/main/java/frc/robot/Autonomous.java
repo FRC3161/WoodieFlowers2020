8d94c64c92d2b1f204c61b75cb79f1323dd8e55b
David Drobner <david@drobner.ca>
2020-02-14 22:10:28 -0500

    Added a Ball argument to Autonomous' constructor

--- src/main/java/frc/robot/Autonomous.java
+++ src/main/java/frc/robot/Autonomous.java
@@ -1,13 +1,16 @@
 package frc.robot;
 
 import frc.robot.subsystems.drivetrain.Drivetrain;
+import frc.robot.subsystems.ball.Ball;
 
 public class Autonomous {
     
     Drivetrain drivetrain;
+    Ball ball;
     
-    Autonomous(Drivetrain driveSubsystem) {
+    Autonomous(Drivetrain driveSubsystem, Ball ballSubsystem) {
         this.drivetrain = driveSubsystem;
+        this.ball = ballSubsystem;
     }
 
     public void hitNRun() {
