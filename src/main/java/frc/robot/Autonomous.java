b2fcbf4407af0479c4d12240ee3786dda7cef3d3
David Drobner <david@drobner.ca>
2020-02-14 22:58:04 -0500

    Made the while loop in hitNRun only run until the robot is at the setpoint

--- src/main/java/frc/robot/Autonomous.java
+++ src/main/java/frc/robot/Autonomous.java
@@ -20,7 +20,7 @@ public class Autonomous {
         Thread.sleep(5000);
         this.ball.cancelShooting();
         this.drivetrain.setSetpoint(-120 * (WHEEL_DIAMETER * 128)); // Distance in inches * Wheel diameter in inches * ticks per rotation
-        while(true){
+        while(!this.drivetrain.atSetpoint()){
             this.drivetrain.drivePID();
         }
     }
