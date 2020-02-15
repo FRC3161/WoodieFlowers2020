7b595a430e2e2d9148b797502ac2c4c96d2b2017
David Drobner <david@drobner.ca>
2020-02-14 22:07:10 -0500

    Created Autonomous class

---
+++ src/main/java/frc/robot/Autonomous.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+import frc.robot.subsystems.drivetrain.Drivetrain;
+
+public class Autonomous {
+    
+    Drivetrain drivetrain;
+    
+    Autonomous(Drivetrain driveSubsystem) {
+        this.drivetrain = driveSubsystem;
+    }
+
+    public void hitNRun() {
+
+    }
+
+}
