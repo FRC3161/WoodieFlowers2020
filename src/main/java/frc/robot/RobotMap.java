fd8a2e7ac7411d6ae028b05a2fe42376e0882439
David Drobner <david@drobner.ca>
2020-02-20 19:44:39 -0500

    Update motor controller ports in RobotMap

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -4,25 +4,25 @@ public final class RobotMap {
     // TODO gotta fix all of these, it's pretty much just random numbers at this point
     
     // Drive
-    public static final int[] TALON_LEFT_DRIVE_PORTS = {0, 1, 2}; 
-    public static final int[] TALON_RIGHT_DRIVE_PORTS = {3, 4, 5}; 
+    public static final int[] TALON_LEFT_DRIVE_PORTS = {10, 2, 8}; 
+    public static final int[] TALON_RIGHT_DRIVE_PORTS = {9, 5, 6}; 
     public static final int DRIVER_PAD_PORT = 0;
     public static final int OPERATOR_PAD_PORT = 1;
 
     // Encoders
-    public static final int[] LEFT_ENCODER_PORTS = {0, 1}; 
-    public static final int[] RIGHT_ENCODER_PORTS = {2, 3}; 
+    public static final int[] LEFT_ENCODER_PORTS = {2, 3}; 
+    public static final int[] RIGHT_ENCODER_PORTS = {1, 0}; 
 
     // Shooter
-    public static final int[] SHOOTER_TALON_PORTS = {2, 3}; 
-    public static final int ROLLER_TALON_PORT = 3;
-    public static final int BELT_TALON_PORT = 4;
+    public static final int[] SHOOTER_TALON_PORTS = {2, 11}; 
+    public static final int ROLLER_TALON_PORT = 0;
+    public static final int BELT_TALON_PORT = 1;
     public static final int[] ULTRASONIC_PORTS = {7,8}; // {IN, OUT}
     public static final int[] SHOOTER_SOLENOID_CHANNELS = {7, 8}; 
     public static final int[] SHOOTER_ENCODER_PORTS = {4, 5};
 
     // Intake
-    public static final int INTAKE_TALON_PORT = 8;
+    public static final int INTAKE_TALON_PORT = 4;
     public static final int INTAKE_SOLENOID_IN_CHANNEL = 0;
     public static final int INTAKE_SOLENOID_OUT_CHANNEL = 3;
 
