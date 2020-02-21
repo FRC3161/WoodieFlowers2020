83e421fc6dc296cb27b5c2db5baf235588c206ef
David Drobner <david@drobner.ca>
2020-02-21 12:18:29 -0500

    Remove obsolete methods from Climber and ClimberImpl

--- src/main/java/frc/robot/subsystems/climb/Climber.java
+++ src/main/java/frc/robot/subsystems/climb/Climber.java
@@ -1,7 +1,6 @@
 package frc.robot.subsystems.climb;
 
 public interface Climber {
-    public void move(char direction);
     void extendClimber();
     void retractClimber();
     void liftRobot();
--- src/main/java/frc/robot/subsystems/climb/ClimberImpl.java
+++ src/main/java/frc/robot/subsystems/climb/ClimberImpl.java
@@ -7,7 +7,6 @@ import frc.robot.RobotMap;
 
 public class ClimberImpl implements Climber {
     
-    WPI_TalonSRX climberMotorController;
     WPI_TalonSRX lifterMotorController;
 
     // Pretty sure there are two solenoids
@@ -15,20 +14,10 @@ public class ClimberImpl implements Climber {
     Solenoid climberSolenoid2;
     
     public ClimberImpl() {
-        this.climberMotorController = new WPI_TalonSRX(RobotMap.CLIMBER_TALON_PORT);
         this.lifterMotorController = new WPI_TalonSRX(RobotMap.LIFTER_TALON_PORT);
 
         this.climberSolenoid1 = new Solenoid(RobotMap.CLIMBER_SOLENOID1_CHANNEL);
         this.climberSolenoid2 = new Solenoid(RobotMap.CLIMBER_SOLENOID2_CHANNEL);
-    }
-
-    public void move(char direction) {
-        // TODO determine which direction is positive and which is negative
-        if(direction == 'L') {
-            this.climberMotorController.set(0.60);
-        } else if(direction == 'R') {
-            this.climberMotorController.set(-0.60);
-        }
     }
 
     // Consider making one method that inverts the climber instead of extending it or retracting it
