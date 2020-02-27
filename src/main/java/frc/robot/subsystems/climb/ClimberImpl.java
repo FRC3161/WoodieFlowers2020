356b1a1ef2e129293ec890f119b22abf5cbe8af5
David Drobner <david@drobner.ca>
2020-02-27 13:07:38 -0500

    Remove extra solenoid from ClimberImpl

--- src/main/java/frc/robot/subsystems/climb/ClimberImpl.java
+++ src/main/java/frc/robot/subsystems/climb/ClimberImpl.java
@@ -11,23 +11,19 @@ public class ClimberImpl implements Climber {
 
     // Pretty sure there are two solenoids
     DoubleSolenoid climberSolenoid1;
-    DoubleSolenoid climberSolenoid2;
     
     public ClimberImpl() {
         this.lifterMotorController = new WPI_TalonSRX(RobotMap.LIFTER_TALON_PORT);
 
-        this.climberSolenoid1 = new DoubleSolenoid(RobotMap.CLIMBER_SOLENOID1_CHANNELS[0], RobotMap.CLIMBER_SOLENOID1_CHANNELS[1]);
-        this.climberSolenoid2 = new DoubleSolenoid(RobotMap.CLIMBER_SOLENOID2_CHANNELS[0], RobotMap.CLIMBER_SOLENOID2_CHANNELS[1]);
+        this.climberSolenoid1 = new DoubleSolenoid(RobotMap.CLIMBER_SOLENOID_CHANNELS[0], RobotMap.CLIMBER_SOLENOID_CHANNELS[1]);
     }
 
     public void extendClimber() {
         this.climberSolenoid1.set(DoubleSolenoid.Value.kForward);
-        this.climberSolenoid2.set(DoubleSolenoid.Value.kForward);
     }
 
     public void retractClimber() {
         this.climberSolenoid1.set(DoubleSolenoid.Value.kReverse);
-        this.climberSolenoid2.set(DoubleSolenoid.Value.kReverse);
     }
 
     public void liftRobot(){
