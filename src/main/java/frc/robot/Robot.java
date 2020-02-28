7935868eb4a727c2e5d1a37892970f1510b482e7
David Drobner <david@drobner.ca>
2020-02-27 23:01:34 -0500

    Retract Climber solenoid on teleop enable

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -111,6 +111,9 @@ public class Robot extends TitanBot {
    */
   @Override
   public void teleopSetup() {
+    
+    this.climb.retractClimber();
+
     // TODO talk with driveteam about controls
     this.drive.driveArcade(this.driverPad.getValue(ControllerBindings.LEFT_STICK, ControllerBindings.Y_AXIS), this.driverPad.getValue(ControllerBindings.RIGHT_STICK, ControllerBindings.X_AXIS));
     if(this.operatorPad.getDpadDirection().equals(ControllerBindings.FEEDER_UP)) {
@@ -135,7 +138,7 @@ public class Robot extends TitanBot {
   @Override
   public void teleopRoutine() {
     //TODO properly bind controls
-    this.drive.driveTank(this.driverPad.getValue(ControllerBindings.LEFT_STICK, ControllerBindings.Y_AXIS), this.driverPad.getValue(ControllerBindings.RIGHT_STICK, ControllerBindings.Y_AXIS)); // Yeah it's shorter the old way, but this way we keep all of the bindings in one place
+    //this.drive.driveTank(this.driverPad.getValue(ControllerBindings.LEFT_STICK, ControllerBindings.Y_AXIS), this.driverPad.getValue(ControllerBindings.RIGHT_STICK, ControllerBindings.Y_AXIS)); // Yeah it's shorter the old way, but this way we keep all of the bindings in one place
     this.drive.driveArcade(this.driverPad.getValue(ControllerBindings.LEFT_STICK, ControllerBindings.Y_AXIS), this.driverPad.getValue(ControllerBindings.RIGHT_STICK, ControllerBindings.X_AXIS));
   }
 
