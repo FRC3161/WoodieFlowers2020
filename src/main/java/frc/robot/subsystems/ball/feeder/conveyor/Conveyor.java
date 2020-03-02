6d5026b109a1f3e11356ea62381f28733ac9175c
David Drobner <david@drobner.ca>
2020-03-02 16:34:53 -0500

    Created skeleton Conveyor interface and implementation

---
+++ src/main/java/frc/robot/subsystems/ball/feeder/conveyor/Conveyor.java
@@ -0,0 +1,8 @@
+package frc.robot.subsystems.ball.feeder.conveyor;
+
+import ca.team3161.lib.robot.LifecycleListener;
+import ca.team3161.lib.robot.subsystem.Subsystem;
+
+public interface Conveyor extends Subsystem, LifecycleListener {
+    // Placeholder
+}
---
+++ src/main/java/frc/robot/subsystems/ball/feeder/conveyor/ConveyorImpl.java
@@ -0,0 +1,32 @@
+package frc.robot.subsystems.ball.feeder.conveyor;
+
+import java.util.concurrent.TimeUnit;
+import ca.team3161.lib.robot.LifecycleEvent;
+import ca.team3161.lib.robot.subsystem.RepeatingPooledSubsystem;
+
+public class ConveyorImpl extends RepeatingPooledSubsystem implements Conveyor {
+
+    ConveyorImpl() {
+        super(1, TimeUnit.SECONDS);
+        // Placeholder
+    }
+
+    @Override
+    public void defineResources() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void task() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void lifecycleStatusChanged(LifecycleEvent previous, LifecycleEvent current) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
