4699c165f8138ca84e2b30e33be68cbf0ebb8e86
David Drobner <david@drobner.ca>
2020-03-03 18:14:02 -0500

    Set initial value of state in ConveyorImpl

--- src/main/java/frc/robot/subsystems/ball/feeder/conveyor/ConveyorImpl.java
+++ src/main/java/frc/robot/subsystems/ball/feeder/conveyor/ConveyorImpl.java
@@ -23,6 +23,7 @@ public class ConveyorImpl extends RepeatingPooledSubsystem implements Conveyor {
         super(1, TimeUnit.SECONDS);
         this.conveyorController = new WPI_TalonSRX(RobotMap.BELT_TALON_PORT);
         this.conveyorController.setInverted(true);
+        this.state = ConveyorState.OFF;
     }
 
     @Override
