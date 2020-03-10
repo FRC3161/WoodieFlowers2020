693128d712cc4413523589c21b3bc82ee680264c
David Drobner <david@drobner.ca>
2020-03-09 22:02:28 -0400

    Remove motor controllers from FeederImpl

--- src/main/java/frc/robot/subsystems/ball/feeder/FeederImpl.java
+++ src/main/java/frc/robot/subsystems/ball/feeder/FeederImpl.java
@@ -1,7 +1,6 @@
 package frc.robot.subsystems.ball.feeder;
 
 import ca.team3161.lib.robot.subsystem.RepeatingPooledSubsystem;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.Ultrasonic;
 import frc.robot.subsystems.ball.feeder.UltrasonicPoller;
 import ca.team3161.lib.utils.SmartDashboardTuner;
@@ -10,9 +9,6 @@ import frc.robot.RobotMap;
 
 public class FeederImpl extends RepeatingPooledSubsystem implements Feeder {
     
-    WPI_TalonSRX beltController;
-    WPI_TalonSRX hopperController;
-
     Ultrasonic feederUltrasonic;
     UltrasonicPoller poller;
     SmartDashboardTuner distanceTuner;
@@ -20,16 +16,12 @@ public class FeederImpl extends RepeatingPooledSubsystem implements Feeder {
     public FeederImpl(){
         super(1, TimeUnit.SECONDS);
         
-        this.beltController = new WPI_TalonSRX(RobotMap.BELT_TALON_PORT);
-        this.hopperController = new WPI_TalonSRX(RobotMap.HOPPER_TALON_PORT);
         this.feederUltrasonic = new Ultrasonic(RobotMap.ULTRASONIC_PORTS[0], RobotMap.ULTRASONIC_PORTS[1]);
         
 
         this.poller = new UltrasonicPoller(feederUltrasonic, 3000, 300);
         this.distanceTuner = new SmartDashboardTuner("Ball Distance", 300, d -> this.poller.setDistance(d));
         
-        this.hopperController.configContinuousCurrentLimit(20, 250);
-        this.hopperController.enableCurrentLimit(false);
     }
 
     public void task() {
@@ -39,14 +31,12 @@ public class FeederImpl extends RepeatingPooledSubsystem implements Feeder {
 
     @Override
     public void defineResources() {
-        // TODO Auto-generated method stub
-
+        require(this.feederUltrasonic);
     }
 
     @Override
     public void prime() {
         // TODO Auto-generated method stub
-
     }
 
     @Override
