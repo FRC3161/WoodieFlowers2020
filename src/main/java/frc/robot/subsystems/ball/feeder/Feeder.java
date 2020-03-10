1abbee736d79af1c222eb734cd8d1e3987c72cfa
David Drobner <david@drobner.ca>
2020-03-09 21:48:12 -0400

    Re-wrote feeder interface

--- src/main/java/frc/robot/subsystems/ball/feeder/Feeder.java
+++ src/main/java/frc/robot/subsystems/ball/feeder/Feeder.java
@@ -3,11 +3,14 @@ package frc.robot.subsystems.ball.feeder;
 import ca.team3161.lib.robot.subsystem.Subsystem;
 
 public interface Feeder extends Subsystem{
-    void enableConveyor();
-    void enableHopper();
-    void unload() throws InterruptedException;
-    void stopAll();
-    void stopConveyor();
-    void stopHopper();
-    void reverseFeeder();
+
+    public enum FeederComponent {
+        CONVEYOR,
+        HOPPER
+    }
+
+    void prime();
+    void unload();
+    void enable(FeederComponent component);
+    void stop(FeederComponent component);
 }
